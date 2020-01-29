2d44d6dc76abc842e1bc02db0ead2bee74c0917b
vallecbg <valioplayofficial@gmail.com>
2020-01-29 23:31:50 +0200

    Implemented routing

--- CarsApp/src/app/app-routing.module.ts
+++ CarsApp/src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Route, RouterModule } from "@angular/router";
+import { SigninComponent } from "./auth/signin/signin.component";
+import { SignupComponent } from "./auth/signup/signup.component";
 
-
-const routes: Routes = [];
+const routes: Route[] = [
+  {
+    path: 'auth',
+    children: [
+      { path: 'signin', component: SigninComponent },
+      { path: 'signup', component: SignupComponent }
+    ]
+  }
+  //TODO: create cars component
+  // {path: "cars", children: [
+  //   {path: "", pathMatch: "full", component:}
+  // ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- CarsApp/src/app/core/navigation/navigation.component.ts
+++ CarsApp/src/app/core/navigation/navigation.component.ts
@@ -7,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
+  dropdownLi : string = "nav-item dropdown";
+  dropdownMenu : string = "dropdown-menu";
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  
+  expand() {
+    this.dropdownLi.endsWith('show') 
+    ? this.dropdownLi = "nav-item dropdown" 
+    : this.dropdownLi = "nav-item dropdown show";
+
+    this.dropdownMenu.endsWith('show')
+    ? this.dropdownMenu = "dropdown-menu"
+    : this.dropdownMenu = "dropdown-menu show";
+  }
+
+  logout(){
+    
+  }
 }
