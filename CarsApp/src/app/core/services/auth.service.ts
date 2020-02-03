71671a0de60ad06e2acd40187143160d3a45280f
vallecbg <valioplayofficial@gmail.com>
2020-02-03 20:20:08 +0200

    Updated security using auth guard

--- CarsApp/src/app/core/services/auth.service.ts
+++ CarsApp/src/app/core/services/auth.service.ts
@@ -8,6 +8,7 @@ import { Router } from "@angular/router";
 })
 export class AuthService {
   private _isAuth: boolean;
+  token: string;
 
   isAuthChanged = new Subject<boolean>();
   constructor(private dbAuth: AngularFireAuth, private router: Router) {}
@@ -28,8 +29,8 @@ export class AuthService {
     this.dbAuth.auth
       .createUserWithEmailAndPassword(email, password)
       .then(data => {
-        this.router.navigate(['/auth/signin']);
-        console.log(data)
+        this.router.navigate(["/auth/signin"]);
+        console.log(data);
       })
       .catch(err => console.error(err));
   }
@@ -38,14 +39,34 @@ export class AuthService {
     this.dbAuth.auth
       .signInWithEmailAndPassword(email, password)
       .then(data => {
-        this.router.navigate(['/']);
-        console.log(data)
+        this.dbAuth.auth
+        .currentUser
+        .getIdToken()
+        .then((token: string) => {
+          this.token = token;
+          sessionStorage.setItem('authtoken', this.token);
+        })
+        this.router.navigate(["/"]);
+        console.log(data);
       })
       .catch(err => console.error(err));
   }
 
   logout() {
     this.dbAuth.auth.signOut();
-    this.router.navigate(['/']);
+    this.router.navigate(["/"]);
+  }
+
+  getUserId() {
+    return this.dbAuth.auth.currentUser ? this.dbAuth.auth.currentUser.uid : "";
+  }
+
+  getToken() {
+    this.token = sessionStorage.getItem('authtoken');
+    return this.token;
+  }
+  
+  isAuth(): boolean{
+    return this.token != null;
   }
 }
